68fa9bb2542656e1c08adc602b513b3aadb74908
경연 <rudyeon@naver.com>
2024-01-27 01:58:25 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,6 @@
+function solution(num_list) {
+    let i = num_list.length-1;
+    num_list[i]>num_list[i-1] ? num_list.push(num_list[i]-num_list[i-1]) : 
+    num_list.push(num_list[i]*2);
+    return num_list;
+}
